ad7930b0315db6ea3344659dbff0be8f2baf1f53
theandrewykim <theandrewkimm@gmail.com>
2016-09-23 14:42:13 -0700

    push

--- app/controllers/chordals_controller.rb
+++ app/controllers/chordals_controller.rb
@@ -4,9 +4,18 @@ class ChordalsController < ApplicationController
 
 
 	def accept_webhook
-	  @body = request.body
+	  @body = request.body.read
+	  verify_signature(@body)
       @headers = request.headers
       p @headers
       render :nothing => true
+      push = JSON.parse(params[:payload])
+  	  puts "chord received #{push.inspect}"
 	end
+
+    def verify_signature(payload_body)
+      signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['HUB_SECRET'], payload_body)
+      return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+    end
+
 end
