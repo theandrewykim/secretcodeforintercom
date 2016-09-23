fea591fb8eaefcacb4c7c1e2dee45fa3295c752d
aykim <theandrewkimm@gmail.com>
2016-09-22 20:27:25 -0700

    skip auth

--- app/controllers/chordals_controller.rb
+++ app/controllers/chordals_controller.rb
@@ -1,9 +1,11 @@
 class ChordalsController < ApplicationController
+  skip_before_filter  :verify_authenticity_token, :authenticate_user!
+
+
 
 	def accept_webhook
 		@body = request.body
     @headers = request.headers
-    flash[:notice] = "message recieved"
     redirect_to root_path
 	end
 end
