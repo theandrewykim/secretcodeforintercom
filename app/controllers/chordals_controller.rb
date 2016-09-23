2309cd40030b3559ad906c6778f3c66d69620400
aykim <theandrewkimm@gmail.com>
2016-09-22 20:07:10 -0700

    flash

--- app/controllers/chordals_controller.rb
+++ app/controllers/chordals_controller.rb
@@ -3,6 +3,7 @@ class ChordalsController < ApplicationController
 	def accept_webhook
 		@body = request.body
     @headers = request.headers
-    flash.now.notice = "message recieved"
+    flash[:notice] = "message recieved"
+    redirect_to root_path
 	end
 end
